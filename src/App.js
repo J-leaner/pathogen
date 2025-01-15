d50f4418382c4691275f57c953294681f84cfd04
Jzh04 <speaker_1@sjtu.edu.cn>
2025-01-15 22:58:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="p-4">
+        <nav className="mb-4">
+          <Link to="/" className="mr-4">Home</Link>
+          <Link to="/search" className="mr-4">Search</Link>
+          <Link to="/phylogenetic-tree">Phylogenetic Tree</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/phylogenetic-tree" element={<PhylogeneticTree />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h1 className="text-xl font-bold">Welcome to Pathogen Genome Database</h1>;
+}
+
+function Search() {
+  return <h1 className="text-xl">Search Page</h1>;
+}
+
+function PhylogeneticTree() {
+  return <h1 className="text-xl">Phylogenetic Tree Visualization</h1>;
 }
 
 export default App;
